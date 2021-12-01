ef9b67a47d3e217c0163d0f8a22c337b4d55d801
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-12-02 00:13:56 +0300

    Add downloading book

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import requests
+
+url = 'https://tululu.org/txt.php?id=32168'
+response = requests.get(url)
+response.raise_for_status()
+with open('book.txt', 'w') as file:
+    file.write(response.text)
+
+
 
