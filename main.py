692ed467abebfa92580b1e8eedba202d465a2d01
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-12-02 23:32:22 +0300

    Add checking redirect

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ import os
 path = 'books/'
 os.makedirs(path, exist_ok=True)
 
-url = 'https://tululu.org/txt.php'
 
-for id in range(1, 11):
-    payload = {
-        'id': id
-    }
+def downoload_books(path, id):
+    url = 'https://tululu.org/txt.php'
+    payload = {'id': id}
     response = requests.get(url, params=payload)
     response.raise_for_status()
+    check_redirect(response.history)
     with open(f'{path}book{id}.txt', 'w') as file:
         file.write(response.text)
+    return response.history
+
+
+def check_redirect(response):
+    if response:
+        raise requests.HTTPError('Redirect to main')
+
+
+for id in range(1, 11):
+    try:
+        response = downoload_books(path, id)
+    except:
+        continue
+
+
