1aafc8b336f99cb98e1490a479e45d9d8b2a2de5
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-12-05 00:03:49 +0300

    Add downloading books with titles

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import requests
 import os
+from pathvalidate import sanitize_filename
+from bs4 import BeautifulSoup
 
-path = 'books/'
-os.makedirs(path, exist_ok=True)
 
-
-def downoload_books(path, id):
-    url = 'https://tululu.org/txt.php'
-    payload = {'id': id}
-    response = requests.get(url, params=payload)
+def title_book(id):
+    url = f'https://tululu.org/b{id}/'
+    response = requests.get(url)
     response.raise_for_status()
-    check_redirect(response.history)
-    with open(f'{path}book{id}.txt', 'w') as file:
-        file.write(response.text)
-    return response.history
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_spec = soup.find('body').find('h1').text
+    return (book_spec.strip().split('::')[0].strip(),
+            book_spec.strip().split('::')[1].strip())
 
 
 def check_redirect(response):
@@ -21,9 +19,25 @@ def check_redirect(response):
         raise requests.HTTPError('Redirect to main')
 
 
+def download_txt(title, folder, id):
+    url = 'https://tululu.org/txt.php'
+    payload = {'id': id}
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    check_redirect(response.history)
+    filepath = os.path.join(folder, f'{id}.{sanitize_filename(title)}.txt')
+    with open(f'{filepath}', 'w') as file:
+        file.write(response.text)
+    return filepath
+
+
+folder = 'books/'
+os.makedirs(folder, exist_ok=True)
 for id in range(1, 11):
     try:
-        response = downoload_books(path, id)
+        title, author = title_book(id)
+        filepath = download_txt(title, folder, id)
+        print(filepath)
     except:
         continue
 
