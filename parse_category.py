436e101aae1f89951688d279dac362e8424ad9dc
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-12-18 00:02:13 +0300

    Get a book links

--- parse_category.py
+++ parse_category.py
@@ -1,13 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
+import pprint
 
 
 url = 'https://tululu.org/l55/'
 response = requests.get(url)
 response.raise_for_status()
 html_page = BeautifulSoup(response.text, 'lxml')
-anchor = html_page.find(class_='d_book').find('a')['href']
-print(urljoin(url, anchor))
+anchors = html_page.find_all(class_='d_book')
+book_urls = [urljoin(url, anchor.find('a')['href']) for anchor in anchors]
+pprint.pprint(book_urls)
 
 
