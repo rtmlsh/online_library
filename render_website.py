8e127199575a44e54b57abe0e15becab3bfd22f7
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-01-15 14:06:36 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,7 +1,7 @@
 import json
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
-from more_itertools import chunked
+from more_itertools import chunked, chunked_even
 
 
 env = Environment(
@@ -12,17 +12,26 @@ env = Environment(
 with open('results/books.json', 'r') as file:
     books_json = file.read()
 
-books = json.loads(books_json)
+content = list(chunked_even(list(chunked(json.loads(books_json), 2)), 10))
 
-def on_reload(books):
+def on_reload(content):
     template = env.get_template('template.html')
-    rendered_page = template.render(books=list(chunked(books, 2)))
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for num, page in enumerate(content, 1):
+        rendered_page = template.render(books=page)
+        with open(f'pages/index{num}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
-on_reload(books)
+on_reload(content)
 
 server = Server()
 server.watch('template.html', on_reload)
 server.serve(root='.')
+
+
+
+# def on_reload(books):
+#     template = env.get_template('template.html')
+#     rendered_page = template.render(books=list(chunked(books, 2)))
+#     with open('index.html', 'w', encoding="utf8") as file:
+#         file.write(rendered_page)
 
